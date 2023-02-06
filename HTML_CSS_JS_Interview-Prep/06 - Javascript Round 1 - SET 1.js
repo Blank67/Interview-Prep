d946157ee8d47c7d12478ad6dee7e5d0871ec11d
Blank67 <hmnshshrm67@gmail.com>
2023-02-06 21:11:08 +0530

    HTML and CSS done

---
+++ HTML_CSS_JS_Interview-Prep/06 - Javascript Round 1 - SET 1.js
@@ -0,0 +1,24 @@
+/*
+Question 1: What is difference between let and const? What is the difference between let and var?
+Ans: The difference between let and const is that let variable can be declared first and initialized later, whereas const variable should be declared and initialized at the same time.
+The difference between let and var is that, let is block scoped and var is function scoped. Also let and var are hoisted differently let is hoisted but will be in temporal dead zone before initialization so if we try to access let before initialization we get a error however we get undefined in case of var.
+
+Question 2 : what is output of this function
+function sample (){
+for (let i=0; i<=5;i++){
+setTimeout (() => Console.log(i), 1000 );
+}
+}
+Ans: 0,1,2,3,4,5 wil be printed at the same time.
+
+Question 3 : what is output of this
+setTimeout(()=>{
+console.log("a");
+},2000)
+console.log("b") ;
+Ans: b and after 2 seconds a will be printed.
+
+Question 4: how can you print a first and b later with. You are not allowed to use any extra setTimeouts?
+Ans: 
+
+*/
